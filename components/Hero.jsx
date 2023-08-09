91e7f218afbfb34a474f163bbade623e8f15d8b6
Aboubacar Sadik <boube.sadik@gmail.com>
2023-08-09 15:09:52 +0100

    #

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
 		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 			<div className="flex flex-col lg:flex-row items-center justify-center h-screen md:h-[500px] lg:h-[630px] gap-4">
 				<div className="flex flex-col items-center lg:items-start justify-center gap-4 lg:flex-1">
-					<h1 className="text-center lg:text-start">Illiassou Issoufou A. H</h1>
+					<h1 className="text-center lg:text-start">Illiassou Issoufou A.</h1>
 					<h3 className="text-redCol text-center lg:text-start">Freelancer</h3>
 					<p className="text-center lg:text-start">
 						Hello! My name is Illiassou Issoufou Abdoul Hayyou, and I am a professional
