38e691cfde164a0052218fccb12539eb282b4fa6
Aboubacar Sadik <boube.sadik@gmail.com>
2023-08-12 07:43:56 +0100

    #

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./app/**/*.{js,jsx}"
-  ],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
