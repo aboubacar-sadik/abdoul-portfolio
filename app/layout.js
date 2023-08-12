91d5bd14616dca59be55e1d9750c322c231a7d94
Aboubacar Sadik <boube.sadik@gmail.com>
2023-08-12 06:54:55 +0100

    Vercel analytic

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Illiassou Issoufou A. H',
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         </header>
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
