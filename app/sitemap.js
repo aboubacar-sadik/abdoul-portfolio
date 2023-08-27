8167fdb428d03b039bfaad31259d4f1e6c21ac80
Aboubacar Sadik <boube.sadik@gmail.com>
2023-08-27 23:54:40 +0100

    Update post page

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,34 +1,10 @@
-// import { getPosts } from "@/api/queries";
-
-import { getCategories, getPostsForSitmap } from "@/api/queries";
-
 export default async function sitemap() {
     const baseUrl = "https://www.abdoulhayyou.com";
-
-    // // Get All Posts from CMS
-    const posts = await getPostsForSitmap()
-    const postsUrls = posts?.map(post => (
-        {
-            url: `${baseUrl}/posts/${post.slug}`,
-            lastModified: new Date()
-        }
-    ))
-
-    const categories = await getCategories()
-    const categoriesUrls =
-        categories?.map((category) => {
-            return {
-                url: `${baseUrl}/posts?category=${category.name}`,
-                lastModified: new Date(),
-            };
-        }) ?? [];
 
     return [
         {
             url: baseUrl,
             lastModified: new Date(),
         },
-        ...postsUrls,
-        ...categoriesUrls
     ];
 }
